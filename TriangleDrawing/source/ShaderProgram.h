57e1aebd20cac2970f689cc0cc9e59c4790d5bbb
Manoj Yerra <manojyerra@gmail.com>
2017-06-01 18:26:48 +0530

    Added ShaderProgram class.

--- TriangleDrawing/source/ShaderProgram.h
+++ TriangleDrawing/source/ShaderProgram.h
@@ -1,220 +1,33 @@
+#ifndef ShaderProgram_H
+#define ShaderProgram_H
+
 #include <GL/glew.h>
-#include <GL/freeglut.h>
-#include <iostream>
 
 #include <string>
 #include <vector>
-#include <iostream>
-#include <fstream>
 using namespace std;
 
 #define glCreateShader glCreateShaderObjectARB
 #define GL_VERTEX_SHADER GL_VERTEX_SHADER_ARB
 #define GL_FRAGMENT_SHADER GL_FRAGMENT_SHADER_ARB
 
-int CreateGlutWindow(char* title, int x, int y, int w, int h);
-void Display();
-
-GLint CreateShaderProgram(char* vertexShaderFilePath, char* fragmentShaderFilePath);
-GLint CompileShader(char* shaderFilePath, GLenum shaderType);
-unsigned int GetFileLength(string filePath);
-
-
-GLuint vertexbuffer;
-GLuint programID;
-
-static const GLfloat g_vertex_buffer_data[] =
+class ShaderProgram
 {
-   -1.0f, -1.0f, 0.0f,
-   1.0f, -1.0f, 0.0f,
-   0.0f,  1.0f, 0.0f,
+private:
+	GLuint programID;
+
+	unsigned int GetFileLength(string filePath);
+	GLint CompileShader(const char* shaderFilePath, GLenum shaderType);
+
+public:
+	ShaderProgram(const char* vertexShaderFilePath, const char* fragmentShaderFilePath);
+	~ShaderProgram();
+
+	void Begin();
+	void End();
 };
 
-
-int main(int argc, char **argv)
-{
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-	CreateGlutWindow("First Window", 0,0,500,500);
-	glewInit();
-
-	programID =  CreateShaderProgram("shaders/vertexshader.txt","shaders/fragmentshader.txt");
-
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-	glutDisplayFunc(Display);
-	glutMainLoop();
-
-	return 0;
-}
-
-
-int CreateGlutWindow(char* title, int x, int y, int w, int h)
-{
-	glutInitWindowPosition(x,y);
-	glutInitWindowSize(w,h);
-	return glutCreateWindow(title);
-}
-
-
-void Display()
-{
-	glClearColor(1,1,1,1);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glUseProgram(programID);
-
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	//glVertexAttribPointer(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const GLvoid* pointer);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-	glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
-	glDisableVertexAttribArray(0);
-
-	glUseProgram(0);
-
-	glutSwapBuffers();
-}
-
-
-GLint CreateShaderProgram(char* vertexShaderFilePath, char* fragmentShaderFilePath)
-{
-	printf("\nCompiling vertex shader...\n");
-	GLint vertexShaderObj = CompileShader(vertexShaderFilePath, GL_VERTEX_SHADER);
-
-	if(!vertexShaderObj)
-		return -1;
-
-	printf("\nCompiling fragment shader...\n");
-	GLint fragmentShaderObj = CompileShader(fragmentShaderFilePath, GL_FRAGMENT_SHADER);
-
-	if(!fragmentShaderObj)
-		return -1;
-
-	// Link the program
-	printf("\nLinking program\n");
-	GLuint programID = glCreateProgram();
-	glAttachShader(programID, vertexShaderObj);
-	glAttachShader(programID, fragmentShaderObj);
-	glLinkProgram(programID);
-
-	// Check the program
-	GLint linkStatus = GL_FALSE;
-	int infoLogLength;
-
-	glGetProgramiv(programID, GL_LINK_STATUS, &linkStatus);
-	glGetProgramiv(programID, GL_INFO_LOG_LENGTH, &infoLogLength);
-	
-	glDetachShader(programID, vertexShaderObj);
-	glDetachShader(programID, fragmentShaderObj);
-	
-	glDeleteShader(vertexShaderObj);
-	glDeleteShader(fragmentShaderObj);
-
-	if(!linkStatus)
-	{
-		printf("\nLinking failed...");
-
-		if(infoLogLength > 0)
-		{
-			std::vector<char> programInfoLog(infoLogLength+1);
-			glGetProgramInfoLog(programID, infoLogLength, NULL, &programInfoLog[0]);
-			printf("%s\n", &programInfoLog[0]);
-		}
-
-		return -1;
-	}
-	else
-	{
-		if(infoLogLength > 0)
-		{
-			std::vector<char> programInfoLog(infoLogLength+1);
-			glGetProgramInfoLog(programID, infoLogLength, NULL, &programInfoLog[0]);
-			printf("%s\n", &programInfoLog[0]);
-		}
-	}
-
-	return programID;
-}
-
-
-unsigned int GetFileLength(string filePath)
-{
-	FILE  *fp = fopen(filePath.c_str(), "rb");
-    
-    if(fp)
-    {
-        fseek(fp, 0, SEEK_END);
-        unsigned int fileSize = (unsigned int)ftell(fp);
-        fseek(fp, 0, SEEK_SET);
-        fclose(fp);
-        return fileSize;
-    }
-    
-    return 0;
-}
-
-
-GLint CompileShader(char* shaderFilePath, GLenum shaderType)
-{
-	unsigned int shaderFileLen = GetFileLength( shaderFilePath );
-	
-	if(shaderFileLen == 0)
-	{
-		printf("\nError : Shader file length is zero.\n");
-		return -1;
-	}
-
-	FILE* fp = fopen(shaderFilePath, "rb");
-	char* shaderFileData = NULL;
-
-    if(fp)
-    {
-		shaderFileData = (char*)malloc(shaderFileLen+1);
-		memset(shaderFileData, (int)'\0', shaderFileLen+1);
-		fread(shaderFileData, 1, shaderFileLen, fp);
-		fclose(fp);
-	}
-	else
-	{
-		printf("\nError : Unable to open shader file.\n");
-		return -1;	//unable to open file.
-	}
-
-	GLuint shaderObject =  glCreateShader(shaderType);
-	glShaderSourceARB(shaderObject, 1, (const char**)(&(shaderFileData)), NULL);
-	glCompileShader(shaderObject);
-
-	free(shaderFileData);
-
-	GLint shaderCompileStatus = GL_FALSE;
-	int compilationLogLength;
-
-	glGetShaderiv(shaderObject, GL_COMPILE_STATUS, &shaderCompileStatus);
-	glGetShaderiv(shaderObject, GL_INFO_LOG_LENGTH, &compilationLogLength);
-
-	std::vector<char> shaderCompileLog(compilationLogLength+1);
-	glGetShaderInfoLog(shaderObject, compilationLogLength, NULL, &shaderCompileLog[0]);
-
-	if(!shaderCompileStatus)
-	{
-		printf("\nCompilation Failed");
-			
-		if(compilationLogLength > 0)
-			printf("\n%s\n", &shaderCompileLog[0]);
-
-		return -1;
-	}
-	else
-	{
-		if(compilationLogLength > 0)
-			printf("\n%s\n", &shaderCompileLog[0]);
-	}
-
-	return shaderObject;
-}
+#endif
 
 
 /*
---
+++ TriangleDrawing/source/main.cpp
@@ -0,0 +1,62 @@
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "ShaderProgram.h"
+
+int CreateGlutWindow(char* title, int x, int y, int w, int h);
+void Display();
+
+GLuint vertexbuffer;
+
+static const GLfloat g_vertex_buffer_data[] =
+{
+   -1.0f, -1.0f, 0.0f,
+   1.0f, -1.0f, 0.0f,
+   0.0f,  1.0f, 0.0f,
+};
+
+ShaderProgram* shaderProgram;
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	CreateGlutWindow("First Window", 0,0,500,500);
+	glewInit();
+
+	shaderProgram = new ShaderProgram("shaders/vertexshader.txt", "shaders/fragmentshader.txt");
+
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	glutDisplayFunc(Display);
+	glutMainLoop();
+
+	return 0;
+}
+
+int CreateGlutWindow(char* title, int x, int y, int w, int h)
+{
+	glutInitWindowPosition(x,y);
+	glutInitWindowSize(w,h);
+	return glutCreateWindow(title);
+}
+
+void Display()
+{
+	glClearColor(1,1,1,1);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	shaderProgram->Begin();
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	//glVertexAttribPointer(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const GLvoid* pointer);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+	glDisableVertexAttribArray(0);
+
+	shaderProgram->End();
+
+	glutSwapBuffers();
+}
