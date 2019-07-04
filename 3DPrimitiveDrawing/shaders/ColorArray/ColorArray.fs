f0c6f7d20a0479d6faf280463253231c50702172
manojyerra <manojyerra@gmail.com>
2019-07-04 19:02:25 +0530

    issue with the color has been fixed ( which occurred in WebGL , dividing every color channel value with 255.0 )

--- 3DPrimitiveDrawing/shaders/ColorArray/ColorArray.fs
+++ 3DPrimitiveDrawing/shaders/ColorArray/ColorArray.fs
@@ -8,8 +8,5 @@ layout (location = 0) out vec4 outColor;
 
 void main (void)  
 {     
-   outColor = fragColor;
+   outColor = vec4(fragColor.r/255.0, fragColor.g/255.0, fragColor.b/255.0, fragColor.a/255.0);
 }
-
-
-//outColor = vec4(fragColor.r/255.0, fragColor.g/255.0, fragColor.b/255.0, fragColor.a/255.0);
