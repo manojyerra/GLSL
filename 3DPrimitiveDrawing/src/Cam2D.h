de107a072d088ebe42cfccd810adf6e3bd44599d
manojyerra <manojyerra@gmail.com>
2019-07-12 10:21:08 +0530

    Modified Cam2D class.

--- 3DPrimitiveDrawing/src/Cam2D.h
+++ 3DPrimitiveDrawing/src/Cam2D.h
@@ -17,25 +17,13 @@ private:
 	float _right;
 	float _bottom;
 	float _top;
-
 	float _zNear;
 	float _zFar;
-	float _zNearPlaneHalfW;
-
-	glm::vec3 _pivot;
-	glm::vec3 _angle;
-	glm::vec3 _trans;
-
-	bool _isOrtho;
 
 	float _sw;
 	float _sh;
 
-	int _viewType;
-
 	Cam2D();
-
-	void SetProjection();
 
 public:
 	GLMat projMat;
@@ -44,15 +32,13 @@ public:
 	static Cam2D* GetInstance();
 	static void DeleteInstance();
 
-	void Init(int screenW, int screenH, float zNear, float zFar, float zNearPlaneW);
+	void Init(int screenW, int screenH);
 	void SetScreenSize(int sw, int sh);
-
-	void SetOrthoProjection(float left, float right, float bottom, float top, float zNear, float zFar);
 	
 	glm::mat4 GetMVP(float* modelMat);
 	glm::mat4 GetModelView(float* modelMat);
-	glm::mat3 GetNormalMat(float* modelMat);
 	
+	void SetProjection();
 	bool UpdateCamera();
 };
 
