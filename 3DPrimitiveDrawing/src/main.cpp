e9e25ace9aec577066628d7357ef033fa4ed2301
manojyerra <manojyerra@gmail.com>
2019-07-23 14:25:29 +0530

    Code refactoring

--- 3DPrimitiveDrawing/src/main.cpp
+++ 3DPrimitiveDrawing/src/main.cpp
@@ -15,7 +15,6 @@ int main(void)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 	glfwWindowHint(GLFW_DEPTH_BITS, 32);
 	glfwWindowHint(GLFW_ALPHA_BITS, 8);
-	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_SRGB_CAPABLE, GLFW_TRUE);
 
 	//TODO: How to change number of samples runtime and disable/enable sampling runtime ( mainly for while drawing an object ).
@@ -27,7 +26,7 @@ int main(void)
 	int sh = mode->height*0.75;
 
 	GLFWwindow* window = glfwCreateWindow(sw, sh, "GLFW Window", NULL, NULL);
-	//glfwMaximizeWindow(window);
+	glfwSetWindowPos(window, 0, 0);
 	glfwMakeContextCurrent(window);
 	glfwSetMouseButtonCallback(window, mouse_button_callback);
 	glfwSetScrollCallback(window, scroll_callback);
@@ -50,6 +49,9 @@ int main(void)
 	{
 		glfwPollEvents();
 
+		for (int i = 0; i <Input::NUM_KEYS; i++)
+			Input::storeKeyStates[i] = glfwGetKey(window, i);
+
 		double xpos, ypos;
 		glfwGetCursorPos(window, &xpos, &ypos);
 
@@ -62,8 +64,6 @@ int main(void)
 		frameCount++;
 		double currentTime = glfwGetTime();
 		double deltaTime = currentTime - previousTime;
-
-		//Sleep(1);
 
 		if(deltaTime > 0.5)
 		{
@@ -90,21 +90,18 @@ int main(void)
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-	// make sure the viewport matches the new window dimensions; note that width and 
-	// height will be significantly larger than specified on retina displays.
-
-	char arr[1024];
-	sprintf(arr, "\nOnSizeChange : sw = %d sh = %d", width, height);
-	OutputDebugStringA(arr);
+	//char arr[1024];
+	//sprintf(arr, "\nOnSizeChange : sw = %d sh = %d", width, height);
+	//OutputDebugStringA(arr);
 
 	if (gameLoop && width > 10 && height > 10)
 	{
 		gameLoop->SetScreenSize(width, height);
 	}
-	else
-	{
-		OutputDebugStringA("\n\n ***** Ignoring framebuffer size callback. **** \n\n");
-	}
+	//else
+	//{
+	//	OutputDebugStringA("\n\n ***** Ignoring framebuffer size callback. **** \n\n");
+	//}
 }
 
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
@@ -144,7 +141,6 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 	}
 }
 
-
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
 	Input::SCROLL_STATE_STORE = (yoffset < 0) ? Input::SCROLL_DOWN : Input::SCROLL_UP;
@@ -152,31 +148,17 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 
 
 /*
-The action is one of GLFW_PRESS or GLFW_RELEASE.
-
-Mouse button states for named buttons are also saved in per - window state arrays that can be polled with glfwGetMouseButton.
-
-int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
-if (state == GLFW_PRESS)
-{
-	upgrade_cow();
-}
-
-
 glfwWindowHint(GLFW_RED_BITS, mode->redBits);
 glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
 glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
 glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
-
 
 glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 
-
-glfwSetWindowPos(window, 0, 0);
+glfwMaximizeWindow(window);
 
 std::cout << glGetString(GL_VERSION) << std::endl;
-
 */
