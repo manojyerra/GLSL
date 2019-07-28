72c703853d2f88319b58b2c359866cc575e27286
manojyerra <manojyerra@gmail.com>
2019-07-28 13:29:15 +0530

    SSAO changes.

--- 3DPrimitiveDrawing/shaders/SSAOGeometryPassShader/SSAOGeometryPassShader.fs
+++ 3DPrimitiveDrawing/shaders/SSAOGeometryPassShader/SSAOGeometryPassShader.fs
@@ -1,21 +1,13 @@
-#version 450
+#version 330 core
 
 layout (location = 0) out vec3 gPosition;
 layout (location = 1) out vec3 gNormal;
-layout (location = 2) out vec4 gAlbedo;
 
 in vec3 vertexVary;
 in vec3 normalVary;
-in vec2 uvVary;
 
 void main()
-{    
-    // store the fragment position vector in the first gbuffer texture
+{
     gPosition = vertexVary;
-    
-	// also store the per-fragment normals into the gbuffer
     gNormal = normalVary;
-	
-    //gAlbedo.rgb = vec3(0.95);
-	gAlbedo = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- 3DPrimitiveDrawing/shaders/SSAOGeometryPassShader/SSAOGeometryPassShader.vs
+++ 3DPrimitiveDrawing/shaders/SSAOGeometryPassShader/SSAOGeometryPassShader.vs
@@ -1,12 +1,10 @@
-#version 450
+#version 330 core
 
 layout (location = 0) in vec4 vertex;
 layout (location = 1) in vec3 normal;
-layout (location = 2) in vec2 uv;
 
 out vec3 vertexVary;
 out vec3 normalVary;
-out vec2 uvVary;
 
 uniform mat4 mvp;
 uniform mat4 modelView;
@@ -16,7 +14,6 @@ void main()
 {
     vertexVary = vec3(modelView * vertex);
 	normalVary = normalize(normalMat * normal);
-    uvVary = uv;
     
     gl_Position = mvp * vertex;
 }
