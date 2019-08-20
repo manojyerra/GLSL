4d32df75cb57c22332cf25d2ddbd73b7fb595067
manojyerra <manojyerra@gmail.com>
2019-08-20 14:21:41 +0530

    Changes.

--- 3DPrimitiveDrawing/src/GameLoop.cpp
+++ 3DPrimitiveDrawing/src/GameLoop.cpp
@@ -26,28 +26,3 @@ GameLoop::~GameLoop()
 {
 	ModulesManager::DeleteInstance();
 }
-
-
-//glm::vec3 v1(0.0f, 0.0f, 0.0f);
-//glm::vec3 v2(0.0f, 5.0f, 0.0f);
-//glm::vec3 v3(5.0f, 0.0f, 0.0f);
-
-//_triangle = new Triangle(v1, v2, v3);
-
-//_box = new Box(0, 0, 0, 2, 3, 4);
-//_box->SetSize(3, 1, 6);
-//_box->SetPos(-10, 0, -10);
-
-//_cylinder = new Cylinder(0, 0, 0, 3, 2);
-//_cylinder->SetRadius(1.5);
-//_cylinder->SetHeight(2);
-//_cylinder->SetPos(0, 0, -10);
-
-//_cone = new Cone(0, 0, 0, 2, 3);
-//_cone->SetRadius(1.5);
-//_cone->SetHeight(2);
-//_cone->SetPos(-5, 0, -10);
-
-//_sphere = new Sphere(0, 0, 0, 2);
-//_sphere->SetPos(5, 0, 0);
-//_sphere->SetRadius(5);
